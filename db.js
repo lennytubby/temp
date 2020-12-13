d2c45b3ed006cb9396f89017aee36a0080239d7f
lennytubby <lennart@slusny.de>
2020-12-13 14:10:29 +0100

    6

--- db.js
+++ db.js
@@ -17,12 +17,19 @@ data.re.doppelkopf = 0
 data.re.karlchen = false
 
 var re_query = "INSERT INTO Re(Solo, Spieler1, Spieler2, Punkte, Ansage, Absage, Fuchs, Doppelkopf, Karlchen) " +
-    "VALUES (\'" + data.re.solo + "\', \'" + data.re.spieler1 + "\', \'" + data.re.spieler2 + "\', " + data.re.punkte + ", " + data.re.ansage + ", " + data.re.absage + ", " + data.re.fuchs + ", " + data.re.doppelkopf + ", " + data.re.karlchen + ") RETURNING id;"
+    "VALUES (" + null + ", \'" + data.re.spieler1 + "\', \'" + data.re.spieler2 + "\', " + data.re.punkte + ", " + data.re.ansage + ", " + data.re.absage + ", " + data.re.fuchs + ", " + data.re.doppelkopf + ", " + data.re.karlchen + ") RETURNING id;"
 
 console.log(re_query)
 client.connect()
     .then(() => console.log("Connected successfuly"))
     .then(() => client.query(re_query))
-    .then(results => {console.table(JSON.stringify(results));console.log(results)})
+    .then(results => { var re_id = results.rows[0].id; return re_id })
     .catch(e => console.log(e))
     .finally(() => client.end())
+
+/*
+async function insert_data(data) {
+    await client.connect()
+
+}
+*/
