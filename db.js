cad04723a4a9d007756676ec67441c6339ada5ee
lennytubby <lennart@slusny.de>
2020-12-13 12:49:07 +0100

    3

--- db.js
+++ db.js
@@ -16,10 +16,13 @@ data.re.fuchs = 0
 data.re.doppelkopf = 0
 data.re.karlchen = false
 
+var re_query = "INSERT INTO Re (Solo, Spieler1, Spieler2, Punkte, Ansage, Absage, Fuchs, Doppelkopf, Karlchen)" +
+    "VALUES (" + data.re.solo + ", " + data.re.spieler1 + ", " + data.re.spieler2 + ", " + data.re.punkte + ", " + data.re.ansage + ", " + data.re.absage + ", " + data.re.fuchs + ", " + data.re.doppelkopf + ", " + data.re.karlchen + ")"
+
+console.log(re_query)
 client.connect()
     .then(() => console.log("Connected successfuly"))
-    .then(() => client.query("insert into Re (Solo, Spieler1, Spieler2, Punkte, Ansage, Absage, Fuchs, Doppelkopf, Karlchen)" + 
-                        "VALUES (" + data.re.solo + ", " + data.re.spieler1 + ", " + data.re.spieler2 + ", " + data.re.punkte + ", " + data.re.ansage + ", " + data.re.absage + ", " + data.re.fuchs + ", " + data.re.doppelkopf + ", " + data.re.karlchen + ")"))
+    .then(() => client.query(re_query))
     .then(results => console.table(results.rows))
     .catch(e => console.log(e))
     .finally(() => client.end())
