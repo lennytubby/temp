f6ddc8064384d0840852d59baabc2ea56a99f811
root <root@someserver@mail.com>
2021-05-22 19:59:16 +0000

    backup any change

--- backup.sh
+++ backup.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 mkdir -p doppelkopf_backup
-name=$(date +'%d.%m.%Y')
+name=$(date +'%d.%m.%Y')5
 pg_dump -U postgres -w doppelkopf > doppelkopf_backup/$name 
+
+# only keep the backup if something changed
+last=$(ls -t doppelkopf_backup | sed -n 2p)
+if cmp -s "doppelkopf_backup/"$name "doppelkopf_backup/"$last; then
+	rm doppelkopf_backup/$name
+fi
