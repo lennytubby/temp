b459d9c5bdf61c7f98c993e58435b4a1a9bf39b0
lennytubby <lennart@slusny.de>
2021-06-20 12:36:02 +0200

    fehlspiel query

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ const { Pool } = require('pg')
 const pool = new Pool({
     user: "postgres",
     password: "SbotmtWigrm.1",
-    host: "45.9.188.38",
-    //host: "127.0.0.1",
+    //host: "45.9.188.38",
+    host: "127.0.0.1",
     database: "doppelkopf"
 })
 
